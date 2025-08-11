576f181dac7390e1ba209fc9c5bf90343713d0d6
Elhanan Katiei <elhanan@rdn.co.il>
2025-08-11 09:45:44 +0300

    Create infrastructure: adding .env file

--- my-react-app/src/lib/axios.ts
+++ my-react-app/src/lib/axios.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+export const apiKey = import.meta.env.VITE_API_KEY;
+
 const api = axios.create({
-  baseURL: 'https://your-api.com/api',
+  baseURL: apiKey,
 });
 
 export default api;
